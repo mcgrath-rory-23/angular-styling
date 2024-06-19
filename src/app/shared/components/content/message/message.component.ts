28f54b944234791a683f96754e50d1e883933962
Rory McGrath <Rory.McGrath@totalmobile.co.uk>
2024-06-19 12:57:21 +0100

    Added message.component.html

--- src/app/shared/components/content/message/message.component.ts
+++ src/app/shared/components/content/message/message.component.ts
@@ -1,18 +1,18 @@
+// message.component.ts
 import { AfterContentInit, Component, ContentChild, ElementRef } from '@angular/core';
 
 @Component({
-    selector: 'saa-message',
-    templateUrl: './message.component.html',
-    styleUrls: ['./message.component.scss']
+  selector: 'saa-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.scss']
 })
-
 export class MessageComponent implements AfterContentInit {
-    @ContentChild('messageContent') messageContent: ElementRef;
-    isLayout01 = false;
+  @ContentChild('messageContent') messageContent: ElementRef;
+  isLayout01 = false;
 
-    constructor(private hostRef: ElementRef) {}
+  constructor(private hostRef: ElementRef) {}
 
-    ngAfterContentInit(): void {
-        this.isLayout01 = this.hostRef.nativeElement.classList.contains('layout--01');
-    }
+  ngAfterContentInit(): void {
+    this.isLayout01 = this.hostRef.nativeElement.classList.contains('layout--01');
+  }
 }
